d3ba7e221471bcf12ec294e8a5ef4ac2e5d704dd
bianyanran <yeran220@163.com>
2018-04-21 11:22:35 +0800

    1
    1

---
+++ travistest/app.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-simple-router');
+const convert = require('koa-convert');
+const path = require('path');
+const serve = require('koa-static');
+
+app.use(router(_ => {
+    _.get('/', (ctx, next) => {
+        ctx.body = 'this is a koa2 demo';
+    })
+}));
+app.use(convert(serve(path.join(__dirname, './public')))); //静态资源文件
+app.listen(3000, () => {
+    console.log('Server Start');
+});
